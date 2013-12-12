c283f3fa190d6f2b82e34d6e7a635cacdacf9046
kyomic <kyomic@163.com>
2013-12-12 17:03:46 +0800

    111

--- seajs_exapmle/ktest/src/main.js
+++ seajs_exapmle/ktest/src/main.js
@@ -2,24 +2,11 @@ define(function(require) {
 
   
 
-  var aaa = require("store");
   
 
   require("jquery");
-  var ccc = require("./test");
-  console.log("test.value=");
-  console.log(ccc);
-
-  var str = require("./loadtest");
-  console.log("require loadtest.");
-  console.log(str);
-  //str.test();
-
-
-  setTimeout( function(){
-  		console.log("after 2 second.");
-  		console.log(ccc);
-  }, 2000);
-
  
+ var c = require("store");
+ console.log(c)
+ c.write("a", 1 );
 });
